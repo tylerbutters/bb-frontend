837071096164b2e9654b894b7ddde2cd0a904ab5
Tyler Butters <tyler@kanjirocket.com>
2026-05-16 22:40:54 +1200

    finished making dictionarygit add .

--- jmdict/processJMdict.js
+++ jmdict/processJMdict.js
@@ -1,11 +1,5 @@
 import fs from "fs"
 
-// -------------------- load JSON --------------------
-
-const JMdictJSON = JSON.parse(fs.readFileSync("./jmdict/jmdict-eng-common.json", "utf8"))
-
-// -------------------- output --------------------
-
 const output = {
 	adjectives: [],
 	adverbs: [],
@@ -95,125 +89,161 @@ function isSuruVerb(entry) {
 	return posList.includes("vs") || posList.includes("vs-s")
 }
 
-function mapVerbType(pos) {
-	if (pos === "v1") return "ichidan"
-	if (pos === "v1-s") return "kureru"
-	if (pos.startsWith("v5")) return "godan"
-	if (pos.startsWith("v5k-s")) return "iku"
-	if (pos === "vk") return "kuru"
-	if (pos.startsWith("vs")) return "suru"
-	if (pos === "v5aru") return "honorific-aru"
+// -------------------- MAPPING TYPES --------------------
+
+function mapVerbType(wordTag) {
+	if (wordTag === "v1") return "ichidan"
+	if (wordTag === "v1-s") return "kureru"
+	if (wordTag.startsWith("v5")) return "godan"
+	if (wordTag.startsWith("v5k-s")) return "iku"
+	if (wordTag === "vk") return "kuru"
+	if (wordTag.startsWith("vs")) return "suru"
+	if (wordTag === "v5aru") return "honorific-aru"
 	return null
 }
 
-function mapAdjectiveType(pos, kana) {
-	if (pos === "adj-i") {
+function mapAdjectiveType(wordTag, kana) {
+	if (wordTag === "adj-i") {
 		if (kana === "いい") return "ii"
 		return "i-type"
 	}
-	if (pos === "adj-na") return "na-type"
+	if (wordTag === "adj-na") return "na-type"
 	return null
 }
 
-// -------------------- main --------------------
+function mapAffixType(wordTag) {
+	if (wordTag === "pref") return "prefix"
+	if (wordTag === "suf") return "suffix"
+	return null
+}
+
+function mapAuxType(wordTag) {
+	if (wordTag === "aux-v" || wordTag === "aux") return "verb"
+	if (wordTag === "aux-adj") return "adjective"
+	return null
+}
+
+// -------------------- ADD ELEMENTS --------------------
+
+function addVerb(wordTag, word, kana, entry) {
+	const verbType = mapVerbType(wordTag)
+	const auxType = mapAuxType(wordTag)
+	let list
+
+	if (verbType) {
+		//if its a suru verb then add する on the end
+		if (verbType === "suru" && isSuruVerb(entry)) {
+			word += "する"
+			kana += "する"
+		}
+		list = output.verbs
+	} else if (auxType && auxType === "verb") list = output.auxiliaries
+	else return
+
+	list.push({
+		elementType: "verb",
+		verbType,
+		text: word,
+		textKana: kana,
+		stem: getStem(word, verbType),
+		stemKana: getStem(kana, verbType),
+		ending: getEnding(word, verbType),
+		conjugation: null,
+		particle: null,
+	})
+}
+function addAdjective(wordTag, word, kana) {
+	const adjectiveType = mapAdjectiveType(wordTag, kana)
+	const auxType = mapAuxType(wordTag)
+	let list
+
+	if (adjectiveType) list = output.adjectives
+	else if (auxType && auxType === "adjective") list = output.auxiliaries
+	else return
+
+	list.push({
+		elementType: "adjective",
+		adjectiveType,
+		text: word,
+		textKana: kana,
+		stem: adjectiveType === "na-type" ? null : getStem(word, adjectiveType),
+		stemKana: adjectiveType === "na-type" ? null : getStem(kana, adjectiveType),
+		ending: adjectiveType === "na-type" ? null : getEnding(word, adjectiveType),
+		conjugation: null,
+		particle: null,
+	})
+}
+function addAffix(wordTag, word, kana) {
+	const affixType = mapAffixType(wordTag)
+	if (affixType) {
+		output.affixes.push({
+			elementType: "affix",
+			affixType,
+			text: word,
+			textKana: kana,
+		})
+	}
+}
+
+function addCounter(wordTag, word) {
+	if (wordTag === "ctr") {
+		output.counters.push({
+			elementType: "counter",
+			text: word,
+			number: "",
+			particle: null,
+		})
+	}
+}
+
+function addAdverb(wordTag, word, kana) {
+	if (wordTag.startsWith("adv")) {
+		output.adverbs.push({
+			elementType: "adverb",
+			text: word,
+			textKana: kana,
+			particle: null,
+		})
+	}
+}
+
+function addNoun(wordTag, word, kana) {
+	if (wordTag === "n") {
+		output.nouns.push({
+			elementType: "noun",
+			text: word,
+			textKana: kana,
+			prefix: null,
+			suffix: null,
+			particle: null,
+		})
+	}
+}
+// -------------------- MAIN --------------------
 
 function processJMdict() {
-	const LIMIT = 100
-	let count = 0
+	const JMdictJSON = JSON.parse(fs.readFileSync("./jmdict/jmdict-eng-common.json", "utf8"))
 
 	for (const entry of JMdictJSON.words) {
-		if (count >= LIMIT) break
-
 		let word = getFirstKanji(entry)
 		let kana = getFirstKana(entry)
 
 		if (!word) continue
 
-		count++
+		const wordTags = entry.sense?.flatMap((s) => s.partOfSpeech || []) || []
 
-		const posList = entry.sense?.flatMap((s) => s.partOfSpeech || []) || []
-
-		// ---------------- VERBS ----------------
-		for (const pos of posList) {
-			const verbType = mapVerbType(pos)
-			if (!verbType) continue
-
-			//if its a suru verb then add する on the end
-			if (verbType === "suru" && isSuruVerb(entry)) {
-				word += "する"
-				kana += "する"
-			}
-
-			output.verbs.push({
-				elementType: "verb",
-				verbType,
-				text: word,
-				textKana: kana,
-				stem: getStem(word, verbType),
-				stemKana: getStem(kana, verbType),
-				ending: getEnding(word, verbType),
-				conjugation: null,
-				particle: null,
-			})
-
-			break
+		for (const wordTag of wordTags) {
+			addVerb(wordTag, word, kana, entry)
+			addAdjective(wordTag, word, kana)
+			addAffix(wordTag, word, kana)
+			addCounter(wordTag, word)
+			addAdverb(wordTag, word, kana)
+			addNoun(wordTag, word, kana)
 		}
-
-		// ---------------- ADJECTIVES ----------------
-		for (const pos of posList) {
-			const adjectiveType = mapAdjectiveType(pos, kana)
-			if (!adjectiveType) continue
-
-			output.adjectives.push({
-				elementType: "adjective",
-				adjectiveType,
-				text: word,
-				textKana: kana,
-				stem: adjectiveType === "na-type" ? null : getStem(word, adjectiveType),
-				stemKana: adjectiveType === "na-type" ? null : getStem(kana, adjectiveType),
-				ending: adjectiveType === "na-type" ? null : getEnding(word, adjectiveType),
-				conjugation: null,
-				particle: null,
-			})
-
-			break
-		}
-
-		// ---------------- ADVERBS ----------------
-		if (posList.some((p) => p.startsWith("adv"))) {
-			output.adverbs.push({
-				elementType: "adverb",
-				text: word,
-				textKana: kana,
-				particle: null,
-			})
-		}
-
-		// ---------------- NOUNS ----------------
-		if (posList.includes("n")) {
-			output.nouns.push({
-				elementType: "noun",
-				text: word,
-				textKana: kana,
-				prefix: null,
-				suffix: null,
-				particle: null,
-			})
-		}
-
-		// ---------------- DEBUG ----------------
-		console.log("ORIGINAL ENTRY")
-		console.log({
-			word,
-			kana,
-			posList,
-			meaning: entry.sense?.[0]?.gloss?.[0]?.text,
-		})
 	}
 
 	fs.writeFileSync("./jmdict/processed-jmdict.json", JSON.stringify(output, null, 2), "utf8")
-	console.log("TRANSFORMED VERB")
-	console.log(output)
+
 	console.log(`Finished processing JMdict, count: ${count}`)
 }
 
